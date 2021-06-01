5ae71778a3e889a0e0b7119d7433203487cd2833
KimoJi71 <76022851+KimoJi71@users.noreply.github.com>
2021-06-02 03:34:49 +0800

    Delete js directory

--- js/end1-6.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".container").fadeIn(1500);
-    $(".btn").delay(1000).fadeIn(1000);
-})
-
-$("#restart").click(function(){
-    $(location).attr("href", "start.html");
-});
--- js/start.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("button").hide().fadeIn(1500);
-})
-
-$("button").click(function(){
-    $(location).attr("href", "makeSet.html");
-});
