08b5a750af4a046cc97621c4fde91242f0b9a503
KimoJi71 <76022851+KimoJi71@users.noreply.github.com>
2021-06-06 22:43:43 +0800

    Delete js directory

--- js/2-3-1-1.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".container").fadeIn(1500);
-    $(".next").delay(1000).fadeIn(1000);
-})
-
-$("button").click(function(){
-    $(location).attr("href", "2-3-1-1sequel.html");
-});
--- js/setName.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".container").fadeIn(1500);
-})
-
-$("#ok").click(function(){
-    var _name = $("#name").val();
-    window.sessionStorage.setItem("name", _name);
-    window.location.href = "setRelationship.html";
-})
